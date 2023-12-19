ff3e9226f234e0f77bc722244cbbb6a12578f992
I635230 <yugo.susuki@gmail.com>
2023-12-19 19:40:37 +0900

    update: page_titleの追加とヘッダーリンクのアクティブ化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(extra_title = '')
+    base_title = 'Conduit'
+    if extra_title.empty?
+      base_title
+    else
+      extra_title + " ― " + base_title
+    end
+  end
 end
