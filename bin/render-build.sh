d88f23d2c2e0351a729cf3f29cd47cec653d25f4
I635230 <yugo.susuki@gmail.com>
2023-12-20 11:07:00 +0900

    fix: デプロイ用ファイルを修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,10 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+# 通常はこちら
+# bundle exec rails db:migrate
+
+# サンプルデータを本番環境で生成
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
